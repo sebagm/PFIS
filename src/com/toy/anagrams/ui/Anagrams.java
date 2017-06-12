b4df3d36560784082eca566e84bf365dfd3b24af
Seba <Seba@SEBA-PC>
2017-06-12 12:11:09 +0200

    Sebastián García commit

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -261,10 +261,10 @@ public class Anagrams extends JFrame {
 
     private void guessedWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guessedWordActionPerformed
         if (wordLibrary.isCorrect(wordIdx, guessedWord.getText())){
-            feedbackLabel.setText("Correct! Try a new word!");
+            feedbackLabel.setText("¡Correcto! ¡Pruebe una nueva palabra!");
             getRootPane().setDefaultButton(nextTrial);
         } else {
-            feedbackLabel.setText("Incorrect! Try again!");
+            feedbackLabel.setText("¡Incorrecto! ¡Pruebe otra vez!");
             guessedWord.setText("");
         }
 
